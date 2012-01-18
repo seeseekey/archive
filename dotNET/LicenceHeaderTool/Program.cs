d9e40264f79fe7c2dcc7b4bd7c3f2c378f5d85ee
seeseekey <seeseekey@googlemail.com>
2012-01-18 16:10:59 +0100

    Implement functionality.

--- dotNET/LicenceHeaderTool/Program.cs
+++ dotNET/LicenceHeaderTool/Program.cs
@@ -2,13 +2,183 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CSCL;
+using System.IO;
 
 namespace LicenceHeaderTool
 {
 	class Program
 	{
+		static void DisplayHelp()
+		{
+			Console.WriteLine("Licence Header Tool v1.00");
+			Console.WriteLine("(c) 2012 by the seeseekey (http://seeseekey.net)");
+			Console.WriteLine("");
+			Console.WriteLine("Nutzung: LicenceHeaderTool -action -parameters");
+			Console.WriteLine("  z.B. LicenceHeaderTool -setGPLv3 /test seeseekey seeseekey@example.org \"2011, 2012\"");
+			Console.WriteLine("");
+			Console.WriteLine("  -setGPLv3 <-overwrite> <projectPath> <author> <mail> <year>");
+		}
+
+		static List<string> GetFilesFromParameters(Parameters param)
+		{
+			List<string> ret=new List<string>();
+
+			foreach(string i in param.GetNames())
+			{
+				if(i.StartsWith("file"))
+				{
+					ret.Add(param.GetString(i));
+				}
+			}
+
+			return ret;
+		}
+
+		#region Licences
+		public static List<string> GetGPLv3(string filename, string author, string mail, string year)
+		{
+			List<string> ret=new List<string>();
+
+			ret.Add("");
+			ret.Add(String.Format("  {0}", filename));
+			ret.Add("  ");
+			ret.Add("  Author:");
+			ret.Add(String.Format("       {0} <{1}>", author, mail));
+			ret.Add(" ");
+			ret.Add(String.Format("  Copyright (c) {0} by {1}", year, author));
+			ret.Add(" ");
+			ret.Add("  This program is free software: you can redistribute it and/or modify");
+			ret.Add("  it under the terms of the GNU General Public License as published by");
+			ret.Add("  the Free Software Foundation, either version 3 of the License, or");
+			ret.Add("  (at your option) any later version.");
+			ret.Add(" ");
+			ret.Add("  This program is distributed in the hope that it will be useful,");
+			ret.Add("  but WITHOUT ANY WARRANTY; without even the implied warranty of");
+			ret.Add("  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
+			ret.Add("  GNU General Public License for more details.");
+			ret.Add(" ");
+			ret.Add("  You should have received a copy of the GNU General Public License");
+			ret.Add("  along with this program.  If not, see <http://www.gnu.org/licenses/>.");
+
+			return ret;
+		}
+		#endregion
+
+		#region Functions
+		static void ProcessCSharpFile(string filename, string author, string mail, string year, License license)
+		{
+			if(FileSystem.GetFilename(filename.ToLower())=="assemblyinfo.cs")
+			{
+				Console.WriteLine("Skip file {0}", FileSystem.GetFilename(filename));
+				return;
+			}
+
+			bool hasLicenseHeader=false;
+
+			List<string> lines=new List<string>();
+			lines.AddRange(File.ReadAllLines(filename));
+
+			foreach(string line in lines)
+			{
+				if(line!=""&&!line.StartsWith("using"))
+				{
+					hasLicenseHeader=true;
+					break;
+				}
+
+				if(line.StartsWith("using"))
+				{
+					break;
+				}
+			}
+
+			//Add licence header
+			if(hasLicenseHeader==false)
+			{
+				List<string> licenceText=GetGPLv3(filename, author, mail, year);
+
+				//Kommentare hinzufügen
+				for(int i=0; i<licenceText.Count; i++)
+				{
+					licenceText[i]="//"+licenceText[i];
+				}
+
+				licenceText.Add(""); //Leerzeile
+
+				//An Datei anfügen
+				licenceText.AddRange(lines);
+
+				//Datei speichern
+				File.WriteAllLines(filename, licenceText);
+
+				//Ausgabe
+				Console.WriteLine("Add licence header to file {0}", FileSystem.GetFilename(filename));
+			}
+		}
+
+		static void ProcessFiles(string projectPath, string author, string mail, string year, License license)
+		{
+			List<string> files=FileSystem.GetFiles(projectPath, true);
+
+			foreach(string file in files)
+			{
+				string ext=FileSystem.GetExtension(file);
+
+				switch(ext.ToLower())
+				{
+					case "cs":
+						{
+							ProcessCSharpFile(file, author, mail, year, license);
+							break;
+						}
+					default:
+						{
+							Console.WriteLine("Ignore file {0}", FileSystem.GetFilename(file));
+							break;
+						}
+				}
+			}
+		}
+		#endregion
+
 		static void Main(string[] args)
 		{
+			//Parameter auswerten
+			Parameters parameters=null;
+
+			try
+			{
+				parameters=Parameters.InterpretCommandLine(args);
+			}
+			catch
+			{
+				Console.WriteLine("Parameters could't regonized!");
+				Console.WriteLine("");
+				DisplayHelp();
+				return;
+			}
+
+			//Aktion starten
+			if(parameters.GetBool("setGPLv3"))
+			{
+				List<string> files=GetFilesFromParameters(parameters);
+
+				if(files.Count<5) Console.WriteLine("Need more parameters!");
+				else
+				{
+					string projectPath=files[0];
+					string author=files[1];
+					string mail=files[2];
+					string year=files[3];
+
+					ProcessFiles(projectPath, author, mail, year, License.GPLv3);
+				}
+			}
+			else
+			{
+				DisplayHelp();
+			}
 		}
 	}
 }
