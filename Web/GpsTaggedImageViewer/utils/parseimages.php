e454300ad8bad628a2ad2e91876f7e2958eff946
seeseekey <seeseekey@gmail.com>
2013-06-17 20:13:22 +0200

    Korrektur der GPS Berechnung.

--- Web/GpsTaggedImageViewer/utils/parseimages.php
+++ Web/GpsTaggedImageViewer/utils/parseimages.php
@@ -15,18 +15,22 @@
 		   	   $latFirst=split("/", $exif['GPS']['GPSLatitude'][0]);
 			   $latSecond=split("/", $exif['GPS']['GPSLatitude'][1]);
 			   $latThird=split("/", $exif['GPS']['GPSLatitude'][2]);
-			   			   
-			   $latSecond=ltrim(str_replace(".", "", ((intval($latSecond[0])/60))), '0');
-			   $latThird=ltrim(str_replace(".", "", ((intval($latThird[0])/3600))), '0');
-			   $lat=$latFirst[0].".".$latSecond.$latThird;
+			   		
+			   $latFirst=intval($latFirst[0])/intval($latFirst[1]);	   
+			   $latSecond=intval($latSecond[0])/intval($latSecond[1]);
+			   $latThird=intval($latThird[0])/intval($latThird[1]);
+			   
+			   $lat=$latFirst+($latSecond*60+$latThird)/3600;
 			  	   
 		       $lonFirst=split("/", $exif['GPS']['GPSLongitude'][0]);
 			   $lonSecond=split("/", $exif['GPS']['GPSLongitude'][1]);
 			   $lonThird=split("/", $exif['GPS']['GPSLongitude'][2]);
 			   
-			   $lonSecond=ltrim(str_replace(".", "", ((intval($lonSecond[0])/60))), '0');
-			   $lonThird=ltrim(str_replace(".", "", ((intval($lonThird[0])/3600))), '0');
-			   $lon=$lonFirst[0].".".$lonSecond.$lonThird;
+			   $lonFirst=intval($lonFirst[0])/intval($lonFirst[1]);	   
+			   $lonSecond=intval($lonSecond[0])/intval($lonSecond[1]);
+			   $lonThird=intval($lonThird[0])/intval($lonThird[1]);
+			   
+			   $lon=$lonFirst+($lonSecond*60+$lonThird)/3600;
 			   
 			   $format="L.marker([%s, %s]).addTo(map).bindPopup(\"<img id='shownImage' src='images/%s' width='300' height='225' /> \");\n";
 			   $line=sprintf($format, $lat, $lon, $file);
