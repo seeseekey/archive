aae0c763b1ee22a75cd58d89d2b1a5c1cdad2e27
seeseekey <seeseekey@gmail.com>
2013-10-20 17:22:09 +0200

    Some security fixes.

--- Web/Mailer/mailer.php
+++ Web/Mailer/mailer.php
@@ -1,13 +1,19 @@
 <?php
-    $senderName=$_POST["sendername"];;
-    $sender=$_POST["sender"];
     $reciever=$_POST["reciever"];
     $allowedRecieverDomain="example.org";
     $subject=$_POST["subject"];
     $text=$_POST["message"];
-    
+	
+	//Sender
+	$senderName="Mailer";
+    $sender="mailer@example.org";
+	
+	//Check reciever
+	$atCount=substr_count($reciever, "@");
+	if($atCount>1) return;
+	
     if(!(strpos($reciever, "@" . $allowedRecieverDomain)===FALSE))
     {
-        mail($reciever, $subject . " - (" . date("m.d.y - H:m:s") . ")", $text, "From: $senderName <$sender>");
+        mail($reciever, $subject . " - (" . date("d.m.y - H:m:s") . ")", $text, "From: $senderName <$sender>");
     }
 ?>
