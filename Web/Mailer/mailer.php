dfe2d89233d35470d3162825043d925760c20110
seeseekey <seeseekey@gmail.com>
2013-10-20 17:23:30 +0200

    Fix tabs.

--- Web/Mailer/mailer.php
+++ Web/Mailer/mailer.php
@@ -3,15 +3,15 @@
     $allowedRecieverDomain="example.org";
     $subject=$_POST["subject"];
     $text=$_POST["message"];
-	
-	//Sender
-	$senderName="Mailer";
+
+    //Sender
+    $senderName="Mailer";
     $sender="mailer@example.org";
-	
-	//Check reciever
-	$atCount=substr_count($reciever, "@");
-	if($atCount>1) return;
-	
+
+    //Check reciever
+    $atCount=substr_count($reciever, "@");
+    if($atCount>1) return;
+
     if(!(strpos($reciever, "@" . $allowedRecieverDomain)===FALSE))
     {
         mail($reciever, $subject . " - (" . date("d.m.y - H:m:s") . ")", $text, "From: $senderName <$sender>");
